9ffec468f829867ba40743610657c15e20e542f4
Jan Armbrust <jan.armbrust@googlemail.com>
2021-05-13 16:05:34 +0200

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        fawn: "#e7a977",
+        "dark-khaki": "#c7cb85",
+        "peach-crayola": "#ebbe9b",
+      },
+    },
   },
   variants: {
     extend: {},
