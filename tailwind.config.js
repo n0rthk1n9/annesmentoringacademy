8f5098f0db6200c4f1c3839d4af9548f82a21934
Jan Armbrust <jan.armbrust@googlemail.com>
2021-05-22 09:39:44 +0200

    add new colors to tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        fawn: "#e7a977",
-        "dark-khaki": "#c7cb85",
-        "peach-crayola": "#ebbe9b",
+        primary: {
+          blue: "hsl(237, 63%, 64%)",
+          "very-light": "hsl(240, 78%, 98%)",
+          light: "hsl(234, 14%, 74%)",
+          normal: "hsl(233, 13%, 49%)",
+          dark: "hsl(232, 13%, 33%)",
+        },
+      },
+      width: {
+        custom: "31%",
+      },
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
       },
     },
   },
