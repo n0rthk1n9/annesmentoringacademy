243b32a60d552a377da94c7691ee84206722a829
Jan Armbrust <jan.armbrust@googlemail.com>
2021-05-22 11:22:15 +0200

    add api endpoints for checkout

---
+++ pages/api/checkout/session.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2020-08-27",
+});
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { quantity, price } = req.body;
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card", "sepa_debit", "giropay", "sofort"],
+    line_items: [
+      {
+        price,
+        quantity,
+      },
+    ],
+    mode: "payment",
+    success_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${req.headers.origin}/checkout`,
+  });
+  res.status(200).json({ sessionId: session.id });
+};
